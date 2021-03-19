b23184396f880c90b189de98605ea82995ed4c16
Anahi S <is715773@iteso.mx>
2021-03-18 23:11:44 -0600

    added app.js correctly

---
+++ front/index.js
@@ -0,0 +1,17 @@
+const URL = 'https://toneanalyzer-paas-anahi.mybluemix.net/';
+var text;
+
+function analizeText(){
+    // textIn = {};
+    text = document.getElementById("intext").value;
+    console.log(text)
+    axios({
+        method: 'post',
+        url: URL,
+        data:{
+            text : text
+        }
+    })
+    .then(data=>console.log(data))
+    .catch(err=>console.log(err))
+}
