5dd3e2e63d346e289783533479f08df831389149
Anahi S <is715773@iteso.mx>
2021-03-18 23:56:25 -0600

    Display json contents on html

--- front/index.js
+++ front/index.js
@@ -1,5 +1,6 @@
 const URL = 'https://toneanalyzer-paas-anahi.mybluemix.net/';
 var text;
+var resp = document.getElementById("res");
 
 function analizeText(){
     // textIn = {};
@@ -12,6 +13,14 @@ function analizeText(){
             text : text
         }
     })
-    .then(data=>console.log(data))
+    .then(data=>{
+        if(data.data.tones[0] == undefined){
+            resp.innerHTML = "Entrada no valida";
+        }else{
+            console.log(data.data.tones[0])
+            resp.innerHTML = data.data.tones[0].tone_name + " : " + data.data.tones[0].score*100 + "%";
+        }
+    }
+    )
     .catch(err=>console.log(err))
 }
